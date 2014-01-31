86aacd31abe0b30a2165980c594419b0b84bf29d
DomLee <eeue53@bangor.ac.uk>
2014-01-31 13:44:06 +0000

    Track bars
    added the track bars

--- EdgeD/EdgeD.pro
+++ EdgeD/EdgeD.pro
@@ -5,3 +5,8 @@ CONFIG -= qt
 
 SOURCES += main.cpp
 
+win32 {
+LIBS += -Lc:/OpenCV/bin -Lc:/OpenCV/lib -lopencv_core246 -lopencv_highgui246 \
+-lopencv_imgproc246 -lopencv_features2d246 -lopencv_calib3d246
+INCLUDEPATH += c:/OpenCV/include
+}
--- EdgeD/main.cpp
+++ EdgeD/main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
 
 using namespace std;
 
-int main()
+void on_Detect_thresh_change(int x,void *p)
 {
-    cout << "Hello World!" << endl;
-    return 0;
+
+}
+void on_Canny_low_change(int x,void *p)
+{
+
+}
+void on_Canny_high_change(int x,void *p)
+{
+
 }
 
+int main()
+{
+    namedWindow("Window",CV_WINDOW_AUTOSIZE);
+
+    createTrackbar( "Detect_thresh"    ,"controller" ,&thresh   ,100 ,on_Detect_thresh_change   ,&image);
+    createTrackbar( "Canny_low"      ,"controller" ,&low ,255 ,on_Canny_low_change  ,&image);
+    createTrackbar( "Canny_high"  ,"controller" ,&high ,255 ,on_Canny_high_change    ,&image);
+}
