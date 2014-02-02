50ae8822cb02a459447390037cb6d35438329f9c
DomLee <eeue53@bangor.ac.uk>
2014-02-02 13:50:02 +0000

    done and tested!!!!
    i have liked up the space key a cout the prints pause and any other key
    exits.

--- EdgeD/main.cpp
+++ EdgeD/main.cpp
@@ -3,18 +3,22 @@
 #include <opencv/highgui.h>
 
 using namespace std;
+using namespace cv;
 
 void on_Detect_thresh_change(int x,void *p)
 {
-
+    x; // trackbar value
+    p; // pointer to a resorce
 }
 void on_Canny_low_change(int x,void *p)
 {
-
+    x; // trackbar value
+    p; // pointer to a resorce
 }
 void on_Canny_high_change(int x,void *p)
 {
-
+    x; // trackbar value
+    p; // pointer to a resorce
 }
 
 int main(int argc, char** argv)
@@ -22,7 +26,6 @@ int main(int argc, char** argv)
     if (argc <2){
         cout << argc <<endl;
         cout << "ERROR: Video argument missing." <<endl;
-        return -1;
     }
     VideoCapture cap(argv[1]); // open the video file for reading
     if ( !cap.isOpened() )  // if not success, exit program
@@ -30,21 +33,23 @@ int main(int argc, char** argv)
         cout << "Cannot open the video file" << endl;
         return -1;
     }
+
     namedWindow("Window",CV_WINDOW_AUTOSIZE);
 
-    thresh = 0;
-    low = 0;
-    high = 0;
+    int thresh = 0;
+    int low = 0;
+    int high = 0;
 
-    createTrackbar( "Detect_thresh" ,"controller" ,&thresh  ,100 ,on_Detect_thresh_change   ,&image);
-    createTrackbar( "Canny_low"     ,"controller" ,&low     ,255 ,on_Canny_low_change       ,&image);
-    createTrackbar( "Canny_high"    ,"controller" ,&high    ,255 ,on_Canny_high_change      ,&image);
+    createTrackbar( "Detect_thresh" ,"Window" ,&thresh  ,100 ,on_Detect_thresh_change   );
+    createTrackbar( "Canny_low"     ,"Window" ,&low     ,255 ,on_Canny_low_change       );
+    createTrackbar( "Canny_high"    ,"Window" ,&high    ,255 ,on_Canny_high_change      );
 
     while(1)
     {
-        k = waitKey();
+        int k = waitKey();
         if (k==32){    // 32 is the code for 'space bar'
-            pause();     // the function for pausing the video
+            //pause();     // the function for pausing the video
+            cout << "pause" <<endl;
         }
         else {
             return -1; // exits on anything that isnt pausing
