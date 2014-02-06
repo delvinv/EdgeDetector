96d2ea498bebd23df437b3ad20f074f199885a12
DomLee <eeue53@bangor.ac.uk>
2014-02-06 14:40:50 +0000

    circle done
    but can you please try and fix the colour thing!

--- EdgeD/main.cpp
+++ EdgeD/main.cpp
@@ -6,13 +6,15 @@
 using namespace std;
 using namespace cv;
 
-int thresh = 0;
-int LowThreshold = 90;
-int HighThreshold = 161;
+int Threshold = 0;
+double ThresholdD = 0.0;
+int LowThreshold = 60;
+int HighThreshold = 200;
 
 void on_Detect_thresh_change(int x,void *p)
 {
-
+    Threshold = x;
+    ThresholdD = (double) x/100;
 }
 void on_Canny_low_change(int x,void *p)
 {
@@ -42,14 +44,12 @@ int main(int argc, char** argv)
     }
 
     namedWindow("Window",CV_WINDOW_AUTOSIZE);
-
-
-
-    createTrackbar( "Detect_thresh" ,"Window" ,&thresh           ,100 ,on_Detect_thresh_change   );
+    createTrackbar( "Detect_thresh" ,"Window" ,&Threshold        ,100 ,on_Detect_thresh_change   );
     createTrackbar( "Canny_low"     ,"Window" ,&LowThreshold     ,255 ,on_Canny_low_change       );
     createTrackbar( "Canny_high"    ,"Window" ,&HighThreshold    ,255 ,on_Canny_high_change      );
 
     Mat frame;
+    Mat gray;
 
     while(1) {
         bool bSuccess = cap.read(frame); // read a new frame from video
@@ -59,20 +59,35 @@ int main(int argc, char** argv)
             break;
         }
 
+                // === First the circle detection ===
+
+        cvtColor(frame, gray, CV_BGR2GRAY);
+        // smooth it, otherwise a lot of false circles may be detected
+        blur( gray, gray, Size(3,3) );
+        vector<Vec3f> circles;
+        HoughCircles(gray, circles, CV_HOUGH_GRADIENT,
+                     2, 100, HighThreshold, LowThreshold, 30, 3000 );
+
+                // === Then the canny edge ditection ===
+
         // Reduce noise with a kernel 3x3
         blur( frame, frame, Size(3,3) );
-
         // Canny detector
         Canny( frame, frame, LowThreshold, HighThreshold, 3 );
 
-        // Using Canny's output as a mask, we display our result
-        // dst = Scalar::all(0);
+                // === Plotting the circles on the canny ===
 
+        for( int i = 0; i < circles.size(); i++ ) {
+            Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+            int radius = cvRound(circles[i][2]);
+            // draw the circle outline
+            circle( frame, center, radius, Scalar(255,255,255), 3, 8, 0 );
+        }
+
+        // dysplay the frame
         imshow("Window", frame );
 
-        // imshow("Window", frame); //show the frame in "MyVideo" window
-
-
+                // === Key commands ===
 
         int k = waitKey(30);
         if(k == 32) { //wait for 'space' key press for 30 ms. Then pauses
